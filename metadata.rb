a1670c1dd60f4283868c2fe62509fbe25d8d6c2b
Mike Heffner <mikeh@fesnel.com>
2012-01-23 14:14:00 -0500

    Add run_with_env script.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mike@librato.com"
 license          "Apache 2.0"
 description      "Misc. recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 depends 'apt', '~> 1.2.0'
 depends 'collectd', '~> 1.0.2'
@@ -12,3 +12,4 @@ depends 'iptables', '~> 0.9.3'
 recipe "misc::apt_upgrade", "Run apt-get upgrade"
 recipe "misc::ssh_firewall", "Setup firewall rules for SSH"
 recipe "misc::collectd", "Enable basic collectd input plugins"
+recipe "misc::run_with_env", "Add the ability to run with environs"
