f8a3c443c66386669ba4986a7bae4f224a9806f1
Mike Heffner <mikeh@fesnel.com>
2012-01-23 09:07:49 -0500

    Add recipe to setup basic collectd inputs.

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,12 @@ maintainer_email "mike@librato.com"
 license          "Apache 2.0"
 description      "Misc. recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends 'apt', '~> 1.2.0'
+depends 'collectd', '~> 1.0.2'
+depends 'iptables', '~> 0.9.3'
 
 recipe "misc::apt_upgrade", "Run apt-get upgrade"
-recipe "misc::ssh_firewall", "Setup firewall runs for SSH"
+recipe "misc::ssh_firewall", "Setup firewall rules for SSH"
+recipe "misc::collectd", "Enable basic collectd input plugins"
